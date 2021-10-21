ac4ed7db3ef670e3df627964022f9f5183d14269
Brady Buchholz <bradybuchholz@gmail.com>
2021-10-21 18:02:12 -0400

    Everything is done. Just need to add local storage, and better formatting

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var APIKey = "2964b09add70d83c046524fefb1dc721"
 var searchButton = document.getElementById("searchButton")
 var displayDat = document.getElementById("displayData")
+var dropDown = document.querySelector(".dropdown-content")
+console.log(dropDown)
 
-function getApi() {
-    var city = document.getElementById("citySearch").value
+function getApi(city) {
+    // var city = document.getElementById("citySearch").value
+    // var pElement = document.createElement("p")
+    // pElement.innerHTML = city;
+    // dropDown.appendChild(pElement)
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + APIKey;
 
     fetch(queryURL)
@@ -22,7 +27,7 @@ function getApi() {
             document.getElementById("temperature").innerHTML = "Temp: " + data.main.temp + "°F"
             document.getElementById("wind").innerHTML = "Wind: " + data.wind.speed + "MPH"
             document.getElementById("humidity").innerHTML = "Humidity: " + data.main.humidity + "%"
-            //Begins fetching API that contains the Uv Index
+            //Begins fetching API that contains the Uv Index and Five Day Forecast
             var name = data.name;
             var lat = data.coord.lat;
             var lon = data.coord.lon;
@@ -38,63 +43,91 @@ function getApi() {
                     console.log(lon)
                     console.log(data.current.uvi)
                     document.getElementById("uvIndex").innerHTML = "UV Index: " + data.current.uvi
-                   
+
                     // Five Day Forecast Starts Here
                     // Day One
-                    var icon1 = data.daily[0].weather[0].icon
-                    var time1 = data.daily[0].dt
-                    // var dateConverter1 = moment.unix(time1).format("MM/DD/YYYY")
+                    var icon1 = data.daily[1].weather[0].icon
+                    var time1 = data.daily[1].dt
+                    var dateConverter1 = moment.unix(time1).format("MM/DD/YYYY")
                     var url1 = "http://openweathermap.org/img/w/" + icon1 + ".png";
-                    document.getElementById("date1").innerHTML = name + " " + " " + "<img src = " + url1 + ">";
-                     + " " + dateConverter + " " + "<img src = " + url + " >"
-                    document.getElementById("temperature1").innerHTML = "Temp: " + data.daily[0].feels_like.day + "°F"
-                    document.getElementById("wind1").innerHTML = "Wind: " + data.daily[0].wind_speed + "MPH"
-                    document.getElementById("humidity1").innerHTML = "Humidty: " + data.daily[0].humidity + "%"
-                    
+                    document.getElementById("date1").innerHTML = name + dateConverter1 + "<img src = " + url1 + ">";
+                    document.getElementById("temperature1").innerHTML = "Temp: " + data.daily[1].feels_like.day + "°F"
+                    document.getElementById("wind1").innerHTML = "Wind: " + data.daily[1].wind_speed + "MPH"
+                    document.getElementById("humidity1").innerHTML = "Humidty: " + data.daily[1].humidity + "%"
+
                     //Day Two
-                    var icon2 = data.daily[1].weather[0].icon
+                    var icon2 = data.daily[2].weather[0].icon
+                    var time2 = data.daily[2].dt
+                    var dateConverter2 = moment.unix(time2).format("MM/DD/YYYY")
                     var url2 = "http://openweathermap.org/img/w/" + icon2 + ".png";
-                    document.getElementById("date2").innerHTML = name + "<img src = " + url2 + ">";
-                    document.getElementById("temperature2").innerHTML = "Temp: " + data.daily[1].feels_like.day + "°F"
-                    document.getElementById("wind2").innerHTML = "Wind: " + data.daily[1].wind_speed + "MPH"
-                    document.getElementById("humidity2").innerHTML = "Humidty: " + data.daily[1].humidity + "%"
-                    
+                    document.getElementById("date2").innerHTML = name + dateConverter2 + "<img src = " + url2 + ">"
+                    document.getElementById("temperature2").innerHTML = "Temp: " + data.daily[2].feels_like.day + "°F"
+                    document.getElementById("wind2").innerHTML = "Wind: " + data.daily[2].wind_speed + "MPH"
+                    document.getElementById("humidity2").innerHTML = "Humidty: " + data.daily[2].humidity + "%"
+
                     //Day Three
-                    var icon3 = data.daily[2].weather[0].icon
+                    var icon3 = data.daily[3].weather[0].icon
                     var url3 = "http://openweathermap.org/img/w/" + icon3 + ".png";
                     document.getElementById("date3").innerHTML = name + "<img src = " + url3 + ">";
-                    document.getElementById("temperature3").innerHTML = "Temp: " + data.daily[2].feels_like.day + "°F"
-                    document.getElementById("wind3").innerHTML = "Wind: " + data.daily[2].wind_speed + "MPH"
-                    document.getElementById("humidity3").innerHTML = "Humidty: " + data.daily[2].humidity + "%"
+                    document.getElementById("temperature3").innerHTML = "Temp: " + data.daily[3].feels_like.day + "°F"
+                    document.getElementById("wind3").innerHTML = "Wind: " + data.daily[3].wind_speed + "MPH"
+                    document.getElementById("humidity3").innerHTML = "Humidty: " + data.daily[3].humidity + "%"
 
                     //Day Four
-                    var icon4 = data.daily[3].weather[0].icon
+                    var icon4 = data.daily[4].weather[0].icon
                     var url4 = "http://openweathermap.org/img/w/" + icon4 + ".png";
                     document.getElementById("date4").innerHTML = name + "<img src = " + url4 + ">";
-                    document.getElementById("temperature4").innerHTML = "Temp: " + data.daily[3].feels_like.day + "°F"
-                    document.getElementById("wind4").innerHTML = "Wind: " + data.daily[3].wind_speed + "MPH"
-                    document.getElementById("humidity4").innerHTML = "Humidty: " + data.daily[3].humidity + "%"
+                    document.getElementById("temperature4").innerHTML = "Temp: " + data.daily[4].feels_like.day + "°F"
+                    document.getElementById("wind4").innerHTML = "Wind: " + data.daily[4].wind_speed + "MPH"
+                    document.getElementById("humidity4").innerHTML = "Humidty: " + data.daily[4].humidity + "%"
 
                     //Day Five
-                    var icon5 = data.daily[4].weather[0].icon
+                    var icon5 = data.daily[5].weather[0].icon
                     var url5 = "http://openweathermap.org/img/w/" + icon5 + ".png";
                     document.getElementById("date5").innerHTML = name + "<img src = " + url5 + ">";
-                    document.getElementById("temperature5").innerHTML = "Temp: " + data.daily[4].feels_like.day + "°F"
-                    document.getElementById("wind5").innerHTML = "Wind: " + data.daily[4].wind_speed + "MPH"
-                    document.getElementById("humidity5").innerHTML = "Humidty: " + data.daily[4].humidity + "%"
-                    
-                    //Changing index color based on conditions
-                    var index = document.getElementById("uvIndex").innerHTML = "UV Index: " + data.current.uvi
+                    document.getElementById("temperature5").innerHTML = "Temp: " + data.daily[5].feels_like.day + "°F"
+                    document.getElementById("wind5").innerHTML = "Wind: " + data.daily[5].wind_speed + "MPH"
+                    document.getElementById("humidity5").innerHTML = "Humidty: " + data.daily[5].humidity + "%"
+
+                    //UV Index Color
+                    var index = parseInt(data.current.uvi)
                     console.log(index)
-                    if (index == 0) {
+                    if (index < 3) {
                         console.log("It's Working")
-                    } else if (index > 0) {
+                        document.getElementById('uvIndex').style.backgroundColor = 'green';
+                    } else if (index < 5) {
+                        document.getElementById("uvIndex").style.backgroundColor = 'yellow';
                         console.log("Howdy")
                     }
+                    else if (index < 7) {
+                        document.getElementById('uvIndex').style.backgroundColor = 'orange';
+                    }
+                    else if (index < 10) {
+                        document.getElementById('uvIndex').style.backgroundColor = 'red';
+                    }
+                    else {
+                        document.getElementById('uvIndex').style.backgroundColor = 'purple';
+
+                    }
                 })
-        })}
-        
-        searchButton.addEventListener("click", getApi)
-       
+        })
+}
+
+// var userInput = document.querySelector("#citySearch")
+// userInput.textContent = saveCity;
+// var saveCity = document.
+
+searchButton.addEventListener("click", function () {
+    var city = document.getElementById("citySearch").value
+    var pElement = document.createElement("p")
+    pElement.innerHTML = city;
+    pElement.addEventListener ('click', function(){
+        console.log(this.innerHTML)
+        getApi(this.innerHTML)
+    })
+    dropDown.appendChild(pElement)
+    getApi(city);
+})
+
 
 
